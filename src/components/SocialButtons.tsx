431f0e12853954c2deddcd3a4a2863e8138ec5bf
shobhitgupta2 <shobhitgupta2003@gmail.com>
2025-02-10 04:10:16 +0530

    made social buttons more responsive

--- src/components/SocialButtons.tsx
+++ src/components/SocialButtons.tsx
@@ -4,9 +4,9 @@ import LeetCodeLogo from "../assets/LeetCodeLogo.tsx";
 
 const SocialButtons = () => {
   return (
-    <div className="flex flex-row gap-10">
+    <div className="flex flex-row lg:gap-10 md:gap-4 gap-2">
       <a
-        className="h-20 w-20 p-1 rounded-3xl bg-zinc-700 border-2 border-transparent flex items-center justify-center
+        className="aspect-square lg:h-20 lg:w-20 md:h-16 md:w-16 h-12 w-12 p-1 rounded-3xl bg-zinc-700 border-2 border-transparent flex items-center justify-center
       hover:bg-zinc-900 hover:border-green-400 transition ease-in-out duration-300"
         href="https://github.com/shobhitgupta2"
         target="_blank"
@@ -14,7 +14,7 @@ const SocialButtons = () => {
         <GitHubLogo className="h-4/5 w-4/5" />
       </a>
       <a
-        className="h-20 w-20 p-1 rounded-3xl bg-zinc-700 border-2 border-transparent flex items-center justify-center
+        className="aspect-square lg:h-20 lg:w-20 md:h-16 md:w-16 h-12 w-12 p-1 rounded-3xl bg-zinc-700 border-2 border-transparent flex items-center justify-center
       hover:bg-zinc-900 hover:border-green-400 transition ease-in-out duration-300"
         href="https://www.linkedin.com/in/shobhit-gupta-81928a249/"
         target="_blank"
@@ -22,7 +22,7 @@ const SocialButtons = () => {
         <LinkedInLogo className="h-4/5 w-4/5" />
       </a>
       <a
-        className="h-20 w-20 p-1 rounded-3xl bg-zinc-700 border-2 border-transparent flex items-center justify-center
+        className="aspect-square lg:h-20 lg:w-20 md:h-16 md:w-16 h-12 w-12 p-1 rounded-3xl bg-zinc-700 border-2 border-transparent flex items-center justify-center
       hover:bg-zinc-900 hover:border-green-400 transition ease-in-out duration-300"
         href="https://leetcode.com/u/shobhitgupta2003/"
         target="_blank"
