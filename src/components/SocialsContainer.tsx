f216d46fe2626b1ac68c432eae72142072c3d8b6
shobhitgupta2 <shobhitgupta2003@gmail.com>
2025-02-10 04:08:34 +0530

    made socials container more responsive

--- src/components/SocialsContainer.tsx
+++ src/components/SocialsContainer.tsx
@@ -3,7 +3,7 @@ import SocialButtons from "./SocialButtons.tsx";
 const SocialsContainer = () => {
   return (
     <div className="w-2/5 h-fit flex flex-col">
-      <h2 className="w-full text-3xl font-[courier] text-neutral-300 mb-12">
+      <h2 className="w-full xl:text-3xl lg:text-2xl text-xl font-[courier] text-neutral-300 mb-12">
         Feel free to send me a message or see me on other platforms
       </h2>
       <SocialButtons />
