f2127792fa028b3054a68a58d0f2bff7636a2cb9
shobhitgupta2 <shobhitgupta2003@gmail.com>
2025-02-10 04:23:06 +0530

    added responsive layout to contact section

--- src/components/SocialsContainer.tsx
+++ src/components/SocialsContainer.tsx
@@ -2,8 +2,8 @@ import SocialButtons from "./SocialButtons.tsx";
 
 const SocialsContainer = () => {
   return (
-    <div className="w-2/5 h-fit flex flex-col">
-      <h2 className="w-full xl:text-3xl lg:text-2xl text-xl font-[courier] text-neutral-300 mb-12">
+    <div className="lg:w-2/5 w-3/5 lg:p-0 items-center h-fit flex flex-col">
+      <h2 className="w-full xl:text-3xl lg:text-2xl text-xl font-[courier] lg:text-left text-center text-neutral-300 mb-12">
         Feel free to send me a message or see me on other platforms
       </h2>
       <SocialButtons />
