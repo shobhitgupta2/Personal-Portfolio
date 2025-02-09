10406b5c07a9546e7360653fa10d795c69636192
shobhitgupta2 <shobhitgupta2003@gmail.com>
2025-02-10 05:20:45 +0530

    tweaked skills and tech array

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,7 +3,7 @@ import { forwardRef } from "react";
 
 const Skills = forwardRef<HTMLDivElement | null>(({}, ref) => {
   return (
-    <div ref={ref} className="w-full min-h-full flex-col content-start">
+    <div ref={ref} className="w-full h-screen flex-col content-start">
       <h2 className="flex w-full xl:text-7xl text-5xl font-[courier] text-green-400 items-center flex-col">
         Skills
       </h2>
--- src/components/TechArray.tsx
+++ src/components/TechArray.tsx
@@ -16,11 +16,11 @@ const TechArray = () => {
   ];
 
   return (
-    <div className="grid grid-cols-3 gap-0 w-3/4 justify-self-center min-h-1/3 justify-items-center">
+    <div className="grid md:grid-cols-3 grid-cols-2 gap-0 w-3/4 justify-self-center h-fit justify-items-center">
       {frontendLogos.map((Logo, index) => (
         <div
           key={index}
-          className="2xl:w-56 2xl:h-56 lg:w-48 lg:h-48 sm:w-36 sm:h-36 w-24 h-24
+          className="lg:w-56 lg:h-56 sm:w-48 sm:h-48 w-32 h-32
           my-10 xl:p-4 md:p-2 p-1 rounded-xl
         flex items-center justify-center
         grayscale hover:grayscale-0
