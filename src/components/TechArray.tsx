f311ed5b2f2ac54191be008cd78f23ad7191bf9d
shobhitgupta2 <shobhitgupta2003@gmail.com>
2025-02-10 06:35:58 +0530

    tech array alignment tweak for responsiveness

--- src/components/TechArray.tsx
+++ src/components/TechArray.tsx
@@ -16,7 +16,7 @@ const TechArray = () => {
   ];
 
   return (
-    <div className="grid md:grid-cols-3 grid-cols-2 gap-0 w-3/4 justify-self-center h-fit justify-items-center">
+    <div className="grid md:grid-cols-3 grid-cols-2 gap-0 md:w-3/4 w-full justify-self-center h-fit justify-items-center">
       {frontendLogos.map((Logo, index) => (
         <div
           key={index}
