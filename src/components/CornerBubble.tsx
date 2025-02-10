ab14a3892194aac2f0c4aff27f55705947ccbaf9
shobhitgupta2 <shobhitgupta2003@gmail.com>
2025-02-10 06:46:23 +0530

    corner button tweak

--- src/components/CornerBubble.tsx
+++ src/components/CornerBubble.tsx
@@ -8,14 +8,13 @@ const CornerBubble = ({ handleClick }: CornerButtonProps) => {
   return (
     <button
       className="w-12 h-12 bg-green-500 rounded-2xl
-    justify-self-end fixed bottom-4 right-9
-    justify-items-center text-gray-100
+    justify-self-end fixed bottom-4 right-9 text-gray-100
     hover:bg-green-400 hover:text-white hover:shadow-green-500
     transition ease-in-out duration-300
     shadow-sm shadow-green-400"
       onClick={handleClick}
     >
-      <ArrowUp className="size-6 stroke-2" />
+      <ArrowUp className="size-6 stroke-2 justify-self-center" />
     </button>
   );
 };
