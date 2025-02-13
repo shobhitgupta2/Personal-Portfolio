f37b2d2097eb1a426b3bdad587c26a026b2b2359
shobhitgupta2 <shobhitgupta2003@gmail.com>
2025-02-14 01:41:31 +0530

    changed some border touching issues on mobile

--- src/components/SocialsContainer.tsx
+++ src/components/SocialsContainer.tsx
@@ -3,10 +3,12 @@ import SocialButtons from "./SocialButtons.tsx";
 const SocialsContainer = () => {
   return (
     <div className="lg:w-2/5 w-3/5 lg:p-0 items-center h-fit flex flex-col">
-      <h2 className="w-full xl:text-3xl lg:text-2xl text-xl font-[courier] lg:text-left text-center text-neutral-300 mb-12">
-        Feel free to send me a message or see me on other platforms
+      <h2 className="w-full xl:text-3xl lg:text-2xl text-lg font-[courier] lg:text-left text-center text-neutral-300 mb-12">
+        Feel free to send me a message or see me on other platforms:
       </h2>
-      <SocialButtons />
+      <div className="items-center flex flex-col">
+        <SocialButtons />
+      </div>
     </div>
   );
 };
