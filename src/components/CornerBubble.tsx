fb816ad3d8d75b7aa6af8e3193dee9c44176e178
shobhitgupta2 <shobhitgupta2003@gmail.com>
2025-02-14 00:42:01 +0530

    made changes to corner button alignment for mobile

--- src/components/CornerBubble.tsx
+++ src/components/CornerBubble.tsx
@@ -8,13 +8,14 @@ const CornerBubble = ({ handleClick }: CornerButtonProps) => {
   return (
     <button
       className="w-12 h-12 bg-green-500 rounded-2xl
-    justify-self-end fixed bottom-4 right-9 text-gray-100
+    fixed bottom-4 md:right-9 right-4
+    justify-items-center items-center
     hover:bg-green-400 hover:text-white hover:shadow-green-500
     transition ease-in-out duration-300
     shadow-sm shadow-green-400"
       onClick={handleClick}
     >
-      <ArrowUp className="size-6 stroke-2 justify-self-center" />
+      <ArrowUp className="w-6 h-6 stroke-2 text-gray-100" />
     </button>
   );
 };
